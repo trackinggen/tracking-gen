abdd7a8275f8d09d7c92dd44b8f474152615548e
Henrique Pereira <henriqueirineu10@hotmail.com>
2023-12-27 14:30:59 -0300

    add proxy integration

--- server.js
+++ server.js
@@ -8,22 +8,84 @@ const hostname = 'localhost'
 const port = process.env.PORT || 8080
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), '/index.html'))
 })
 
-app.get('/melhorrastreio/:baseCode/:range?', (req, res) => {
+app.post('/melhorrastreio/:baseCode/:range?', (req, res) => {
   const baseCode = req.params.baseCode
   const range = req.params?.range || 0
   const codes = genMultiple({ code: baseCode, range })
   const results = []
+  const proxyList = req.body?.proxyList || []
+  const proxies = []
+  let proxyIndex = 0
+  let tries = 0
 
-  const promises = codes.map((code) => {
+  const createProxyObject = (string) => {
+    const urlObj = new URL(string)
+
+    return {
+      host: urlObj.hostname,
+      port: urlObj?.port,
+      auth: urlObj?.username
+        ? { username: urlObj.username, password: urlObj.password }
+        : undefined,
+      protocol: urlObj.protocol.replace(':', ''),
+    }
+  }
+
+  proxyList.forEach((proxyString, index) => {
+    proxies.push(createProxyObject(proxyString))
+  })
+
+  console.log(`Proxy: ${JSON.stringify(proxies?.[proxyIndex] || {})}`)
+
+  const promiseFactory = (code) => {
     return new Promise((resolve, reject) => {
+      console.log(`Running query for code ${code}`)
       axios
         .post('https://api.melhorrastreio.com.br/graphql', {
-          query:
-            'mutation searchParcel ($tracker: TrackerSearchInput!) {\n  result: searchParcel (tracker: $tracker) {\n    id\n    createdAt\n    updatedAt\n    lastStatus\n    lastSyncTracker\n    pudos {\n      type\n      trackingCode\n    }\n    trackers {\n      type\n      shippingService\n      trackingCode\n    }\n    trackingEvents {\n      trackerType\n      trackingCode\n      createdAt\n      translatedEventId\n      originalTitle\n      to\n      from\n    }\n    pudoEvents {\n      trackingCode\n      createdAt\n      translatedEventId\n      originalTitle\n      from\n      to\n      pudoType\n    }\n  }\n}',
+          proxy: { ...proxies?.[proxyIndex] },
+          query: `mutation searchParcel ($tracker: TrackerSearchInput!) {
+            result: searchParcel (tracker: $tracker) {
+              id
+              createdAt
+              updatedAt
+              lastStatus
+              lastSyncTracker
+              pudos {
+                type
+                trackingCode
+              }
+              trackers {
+                type
+                shippingService
+                trackingCode
+              }
+              trackingEvents {
+                trackerType
+                trackingCode
+                createdAt
+                translatedEventId
+                originalTitle
+                to
+                from
+                additionalInfo
+              }
+              pudoEvents {
+                trackingCode
+                createdAt
+                translatedEventId
+                originalTitle
+                from
+                to
+                pudoType
+              }
+            }
+          }`,
           variables: {
             tracker: {
               trackingCode: code,
@@ -33,6 +95,7 @@ app.get('/melhorrastreio/:baseCode/:range?', (req, res) => {
         })
         .then((r) => {
           const object = r.data.data.result
+          // console.log('request index: ', index)
           if (object) {
             const objectPostedActivity = object.trackingEvents.find(
               (e) => e.originalTitle == 'Objeto postado'
@@ -49,26 +112,41 @@ app.get('/melhorrastreio/:baseCode/:range?', (req, res) => {
           resolve()
         })
         .catch((e) => {
-          reject(e)
+          if (e.response.status == 429 && tries < 3) {
+            tries += 1
+            console.log(`Retrying for code ${code}, attempt ${tries}`)
+            promiseFactory(code)
+          } else {
+            console.log(`Request to code ${code} failed: ${e.message}`)
+            resolve()
+          }
         })
     })
+  }
+
+  const promises = codes.map((code) => {
+    return promiseFactory(code)
   })
 
-  Promise.all(promises).then(() => {
-    results.sort((a, b) => {
-      return new Date(b.time) - new Date(a.time)
-    })
-    const recentResults = results.filter((e) => {
-      const objectDate = new Date(e.time)
-      const now = new Date(Date.now())
+  Promise.all(promises)
+    .then(() => {
+      results.sort((a, b) => {
+        return new Date(b.time) - new Date(a.time)
+      })
+      const recentResults = results.filter((e) => {
+        const objectDate = new Date(e.time)
+        const now = new Date(Date.now())
 
-      const isOlderThan5Days =
-        +now > objectDate.setDate(objectDate.getDate() + 5)
-      if (!isOlderThan5Days) return e
-    })
+        const isOlderThan5Days =
+          +now > objectDate.setDate(objectDate.getDate() + 5)
+        if (!isOlderThan5Days) return e
+      })
 
-    res.json(recentResults)
-  })
+      res.json(recentResults)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
 })
 
 app.get('/cainiao/:baseCode', (req, res) => {
